fe05448593411fd04c69ffa338a4621183cf2a58
ProNeverDies <devanshtomar2004@gmail.com>
2025-02-18 02:07:43 +0530

    d

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -1,53 +1,10 @@
-// import {cva,type VariantProps} from "class-variance-authority";
-// import { cn } from "@/lib/utils";
-// import { Skeleton } from "./ui/skeleton";
-// import {
-//     Avatar,
-//     AvatarFallback,
-//     AvatarImage,
-// } from "@/components/ui/avatar";
-
-// const avatarSizes=cva(
-//     "",
-//     {
-//         variants:{
-//             size:{
-//                 default:"h-8 w-8",
-//                 lg:"h-14 w-14"
-//             },
-//         },
-//         defaultVariants:{
-//             size:"default"
-//         },
-//     },
-// );
-
-// interface UserAvatarProps
-// extends VariantProps<typeof avatarSizes>{
-//     imageUrl:string;
-//     username:string;
-//     isLive:boolean;
-//     showBadge?:boolean;
-// }
-
-// export const UserAvatar = ({ imageUrl, username, isLive, showBadge,size }: UserAvatarProps) => {
-//     const canShowbadge=showBadge && isLive;
-//     return (
-//         <div className="relative">
-//            <Avatar
-//            className={cn(
-//             isLive && "ring-2 ring-rose-500 border border-background",
-//            )}>
-//             <AvatarImage src={imageUrl} className="object-cover"/>
-//            </Avatar>
-//         </div>
-//     );
-// };
-
+import React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
+
 import { cn } from "@/lib/utils";
-import { Skeleton } from "./ui/skeleton";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { LiveBadge } from "@/components/live-badge";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const avatarSizes = cva("", {
   variants: {
@@ -64,34 +21,44 @@ const avatarSizes = cva("", {
 interface UserAvatarProps extends VariantProps<typeof avatarSizes> {
   imageUrl: string;
   username: string;
-  isLive: boolean;
+  isLive?: boolean;
   showBadge?: boolean;
 }
 
-export const UserAvatar = ({
+interface UserAvatarSkeletonProps extends VariantProps<typeof avatarSizes> {}
+
+export function UserAvatar({
   imageUrl,
   username,
   isLive,
   showBadge,
   size,
-}: UserAvatarProps) => {
+}: UserAvatarProps) {
   const canShowBadge = showBadge && isLive;
 
   return (
     <div className="relative">
       <Avatar
         className={cn(
-          isLive && "ring-2 ring-rose-500 border border-background"
+          isLive && "ring-2 ring-rose-500 border border-background",
+          avatarSizes({ size })
         )}
       >
-        <AvatarImage src={imageUrl} alt={username} className="object-cover" />
-        {/* Fallback Avatar when imageUrl is not available */}
-        <AvatarFallback>{username[0]}</AvatarFallback>
+        <AvatarImage src={imageUrl} className="object-cover" />
+        <AvatarFallback>
+          {username[0]}
+          {username[username.length - 1]}
+        </AvatarFallback>
       </Avatar>
-      {/* Show badge if user is live */}
       {canShowBadge && (
-        <div className="absolute top-0 right-0 rounded-full bg-rose-500 h-2.5 w-2.5 border-2 border-background"></div>
+        <div className="absolute -bottom-3 left-1/2 transform -translate-x-1/2">
+          <LiveBadge />
+        </div>
       )}
     </div>
   );
-};
+}
+
+export function UserAvatarSkeleton({ size }: UserAvatarSkeletonProps) {
+  return <Skeleton className={cn("rounded-full", avatarSizes({ size }))} />;
+}
